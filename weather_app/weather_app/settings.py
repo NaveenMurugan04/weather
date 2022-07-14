a75902d7c8f4e3b98a0b23b26d19c443562b2dbd
@naveen86 <naveen@br.iq>
2022-07-14 17:31:11 +0530

    setting up views

--- weather_app/weather_app/settings.py
+++ weather_app/weather_app/settings.py
@@ -10,7 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
-from pathlib import Path
+from pathlib import Path 
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'weather_app'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'weather_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
---
+++ weather_app/weather_app/views.py
@@ -0,0 +1,22 @@
+import json
+from multiprocessing import context
+from django.http import HttpResponse
+from django.template import loader
+import requests
+
+
+
+def home(request):
+  
+  template = loader.get_template('home.html')
+  weathers_list = fetch_weathers_list().json()
+  
+  print(weathers_list)
+  
+
+  
+  return HttpResponse(template.render())
+
+def fetch_weathers_list():
+     req_url= "https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=51.5&lon=-0.25"
+     return requests.get(req_url)
